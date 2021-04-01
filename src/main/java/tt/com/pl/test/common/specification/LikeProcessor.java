5cb71ec6916d4b34a18b98f6665910793ae93313
TTIT\wojsam <wojsam>
2021-04-01 18:11:54 +0200

    fix like

--- src/main/java/tt/com/pl/test/common/specification/LikeProcessor.java
+++ src/main/java/tt/com/pl/test/common/specification/LikeProcessor.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 import static tt.com.pl.test.common.specification.CriteriaUtils.getPath;
 
@@ -19,6 +20,6 @@ class LikeProcessor implements Processor<Like> {
         String value = (String) field.get(params);
 
         return (Specification<E>) (eventRoot, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(getPath(annotation.field(), eventRoot), annotation.type().toPredicate(value));
+                Objects.isNull(value) ? null :criteriaBuilder.like(getPath(annotation.field(), eventRoot), annotation.type().toPredicate(value));
     }
 }
