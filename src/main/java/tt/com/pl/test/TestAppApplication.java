3847952a27a5ff39341caa3fc3e82fab100e3417
TTIT\wojsam <wojsam>
2021-04-01 17:48:26 +0200

    add liquibase

--- src/main/java/tt/com/pl/test/TestAppApplication.java
+++ src/main/java/tt/com/pl/test/TestAppApplication.java
@@ -2,8 +2,11 @@ package tt.com.pl.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class TestAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
---
+++ src/main/java/tt/com/pl/test/config/LiquibaseConfiguration.java
@@ -0,0 +1,45 @@
+package tt.com.pl.test.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    private final Environment env;
+
+    public LiquibaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(LiquibaseProperties liquibaseProperties,
+                                     DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:liquibase/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setLabels(liquibaseProperties.getLabels());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setRollbackFile(liquibaseProperties.getRollbackFile());
+        if (env.acceptsProfiles(Profiles.of("no-liqui"))) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+        return liquibase;
+    }
+}
